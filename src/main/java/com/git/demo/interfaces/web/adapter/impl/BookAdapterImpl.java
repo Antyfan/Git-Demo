3a53f106befcdcca123addf27c9a8ff924f8f8da
Antyfan <krzychudom@gmail.com>
2024-10-03 20:06:11 +0200

    Add Book adapter for interface layer

---
+++ src/main/java/com/git/demo/interfaces/web/adapter/impl/BookAdapterImpl.java
@@ -0,0 +1,25 @@
+package com.git.demo.interfaces.web.adapter.impl;
+
+import com.git.demo.application.BookService;
+import com.git.demo.domain.model.entity.Book;
+import com.git.demo.interfaces.web.adapter.BookAdapter;
+import com.git.demo.interfaces.web.adapter.mapper.BookMapper;
+import com.git.demo.interfaces.web.dto.BookDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class BookAdapterImpl implements BookAdapter {
+
+    private final BookService bookService;
+    private final BookMapper bookMapper;
+
+    @Override
+    public List<BookDTO> getAllBooksBy(String category) {
+        List<Book> books = bookService.getBooksBy(category);
+        return bookMapper.fromDomainListToDtoList(books);
+    }
+}
