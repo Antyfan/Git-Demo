9114e6bdb4f7dab4769e89bca17431bb7151aecc
Antyfan <krzychudom@gmail.com>
2024-10-03 20:02:46 +0200

    Add Book Service with initial get method impl

---
+++ src/main/java/com/git/demo/application/BookService.java
@@ -0,0 +1,9 @@
+package com.git.demo.application;
+
+import com.git.demo.domain.model.entity.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> getBooksBy(String category);
+}
