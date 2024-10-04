79999c49690c601a43b87313a68e5c1a8a8fc788
Antyfan <krzychudom@gmail.com>
2024-10-04 16:49:14 +0200

    Add Library adapter - empty impl, todo

---
+++ src/main/java/com/git/demo/interfaces/web/adapter/impl/LibraryAdapterImpl.java
@@ -0,0 +1,11 @@
+package com.git.demo.interfaces.web.adapter.impl;
+
+import com.git.demo.interfaces.web.adapter.LibraryAdapter;
+import com.git.demo.interfaces.web.dto.LibraryInfoDTO;
+
+public class LibraryAdapterImpl implements LibraryAdapter {
+    @Override
+    public LibraryInfoDTO getLibraryInfo(String libraryId) {
+        return null;
+    }
+}
