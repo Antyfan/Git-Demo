a55dbfc69aff646399968102da6397dfea9149f2
Antyfan <krzychudom@gmail.com>
2024-10-04 16:55:59 +0200

    Add basic impl to Library Controller

--- src/main/java/com/git/demo/interfaces/web/LibraryController.java
+++ src/main/java/com/git/demo/interfaces/web/LibraryController.java
@@ -1,6 +1,20 @@
 package com.git.demo.interfaces.web;
 
+import com.git.demo.interfaces.web.adapter.LibraryAdapter;
+import com.git.demo.interfaces.web.dto.LibraryInfoDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
 public class LibraryController {
 
-    //there will be implementation of lib controller
+    private final LibraryAdapter libraryAdapter;
+
+    @RequestMapping
+    public LibraryInfoDTO getLibraryInfo(@PathVariable("libraryId") String libId) {
+        return libraryAdapter.getLibraryInfo(libId);
+    }
 }
