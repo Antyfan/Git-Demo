9c6774adbffbe9ddf0beac11101391b6415eff91
Antyfan <krzychudom@gmail.com>
2024-10-06 19:42:26 +0200

    Extend book Controller by update Book method

--- src/main/java/com/git/demo/interfaces/web/BookController.java
+++ src/main/java/com/git/demo/interfaces/web/BookController.java
@@ -2,14 +2,17 @@ package com.git.demo.interfaces.web;
 
 import com.git.demo.interfaces.web.adapter.BookAdapter;
 import com.git.demo.interfaces.web.dto.BookDTO;
+import com.git.demo.interfaces.web.dto.UpdateBootDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
 
 @RestController
 @AllArgsConstructor
@@ -20,5 +23,10 @@ public class BookController {
     @RequestMapping(value = "/books", method = GET)
     public List<BookDTO> getBooksBy(@PathVariable("category") String category) {
         return bookAdapter.getAllBooksBy(category);
+    }
+
+    @RequestMapping(value = "/book/{bookId}", method = PATCH)
+    public BookDTO updateBook(@PathVariable String bookId, @RequestBody UpdateBootDTO updateBootDTO) {
+        return bookAdapter.updateBook(bookId, updateBootDTO);
     }
 }
